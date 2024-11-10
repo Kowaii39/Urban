8c2b55650cf6762c6d822a754932e13d45205b33
Дмитрий <overlord381@gmail.com>
2024-11-10 18:19:17 +0200

    upload

---
+++ variables.py
@@ -0,0 +1,5 @@
+number_of_completed_dz = 12
+number_of_hours_spend =1.5
+course_name = 'Python'
+time_for_one_ask =(number_of_hours_spend / number_of_completed_dz)
+print(course_name, ' всего задач:', number_of_completed_dz, 'затрачено часов:', number_of_hours_spend, 'среднее выполнение', time_for_one_ask, 'часа.' )
