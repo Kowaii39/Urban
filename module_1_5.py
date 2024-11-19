b226254e72b1bbcffad7a72c097556fb2b6d5178
Дмитрий <overlord381@gmail.com>
2024-11-19 18:54:45 +0200

    upload

--- module_1_5.py
+++ module_1_5.py
@@ -5,5 +5,5 @@ print(immutable_var)
 #print(immutable_var) # нельзя менять так так кортеж не поддерживает обращение по элементам
 mutable_list = [1, 2, 3, 'a', 'b', 'c']
 mutable_list.append('d')
-mutable_list[1]= 3
+mutable_list[1] = 3
 print(mutable_list)
